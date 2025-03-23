92a29706cc494e8704a4f7dfa63e290eac185a00
Prof3ssorSt3v3 <griffis@algonquincollege.com>
2025-03-22 23:59:17 -0400

    starter files

---
+++ src/components/header.jsx
@@ -0,0 +1,9 @@
+import styles from './header.module.css';
+
+export default function Header() {
+  return (
+    <header className={styles.header}>
+      <h1>Website Title</h1>
+    </header>
+  );
+}
